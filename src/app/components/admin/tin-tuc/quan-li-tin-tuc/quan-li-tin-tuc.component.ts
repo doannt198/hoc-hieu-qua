6324043c16485b85cd1b29a9a291ac9bc1e062ef
doannt <doannt@unicloud.com.vn>
2022-06-08 00:08:29 +0700

    update

--- src/app/components/admin/tin-tuc/quan-li-tin-tuc/quan-li-tin-tuc.component.ts
+++ src/app/components/admin/tin-tuc/quan-li-tin-tuc/quan-li-tin-tuc.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ConfirmationService, MessageService, PrimeNGConfig } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
+import { ApiService } from 'src/app/service/api.service';
+import * as queryString from 'query-string';
 @Component({
   selector: 'app-quan-li-tin-tuc',
   templateUrl: './quan-li-tin-tuc.component.html',
   styleUrls: ['./quan-li-tin-tuc.component.scss']
 })
 export class QuanLiTinTucComponent implements OnInit {
-
-  constructor() { }
-
+  private readonly unsubscribe$: Subject<void> = new Subject();
+  dataList: any = [];
+  data: any = [];
+  totalRecord = 0;
+  constructor(
+    private apiService: ApiService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
+    private primengConfig: PrimeNGConfig
+  ) { }
+  query = {
+    filter: '',
+    offSet: 0,
+    pageSize: 10,
+  };
   ngOnInit(): void {
+    this.fetchData();
+    this.primengConfig.ripple = true;
+  }
+  fetchData() {
+    this.GetNews();
   }
 
+  GetNews() {
+    const queryParams = queryString.stringify(this.query);
+    this.apiService
+      .GetNews(queryParams)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: (response) => {
+          console.log(response)
+          this.dataList = response.Data.Data;
+          this.totalRecord = response.Data.RecordsTotal;
+        },
+        error: (error) => {
+          console.log('error', error);
+        },
+      });
+  }
+
+  paginate(event: any) {
+    console.log(event);
+    this.query.offSet = event.first;
+    this.query.pageSize = event.rows;
+    this.GetNews();
+  }
+  deleteProduct(Id: string) {
+    
+  }
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -11,7 +11,7 @@ export class ApiService {
 
     /* Api danh mục tin tức  */
     GetNewsCategory(queryParams: any): Observable<any> { 
-        return this.http.get(`https://hhq.somee.com/api/NewsCategory?${queryParams}`)
+        return this.http.get(`https://hhq.somee.com/api/NewsCategory?`+ queryParams)
     }
 
     DeleteCategory(queryParams: any): Observable<any> {
@@ -19,7 +19,11 @@ export class ApiService {
     }
 
     PostCategory(queryParams:any): Observable<any> {
-      return this.http.post(`https://hhq.somee.com/api/NewsCategory`, queryParams)
+      return this.http.post(`https://hhq.somee.com/api/NewsCategory`, queryParams )
     }
+    /* Api quản lí tin tức */ 
 
+    GetNews(queryParams: any):Observable<any> {
+      return this.http.get(`https://hhq.somee.com/api/News?`+ queryParams)
+    }
 }
