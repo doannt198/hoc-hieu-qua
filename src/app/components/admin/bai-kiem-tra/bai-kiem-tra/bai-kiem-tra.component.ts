e09271727c09e03bfb73e002b4d8b71896d2ab25
doannt <doannt@unicloud.com.vn>
2022-06-09 14:13:26 +0700

    update

--- src/app/components/admin/bai-kiem-tra/bai-kiem-tra/bai-kiem-tra.component.ts
+++ src/app/components/admin/bai-kiem-tra/bai-kiem-tra/bai-kiem-tra.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ConfirmationService, MessageService, PrimeNGConfig } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
+import { ApiService } from 'src/app/service/api.service';
+import * as queryString from 'query-string';
 @Component({
   selector: 'app-bai-kiem-tra',
   templateUrl: './bai-kiem-tra.component.html',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BaiKiemTraComponent implements OnInit {
 
-  constructor() { }
+  private readonly unsubscribe$: Subject<void> = new Subject();
+  dataList: any = [];
+  data: any = [];
+  totalRecord = 0;
+  constructor(
+    private apiService: ApiService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
+    private primengConfig: PrimeNGConfig
+  ) { }
+  query = {
+    filter: '',
+    offSet: 0,
+    pageSize: 5,
+    screen: ''
+  };
 
   ngOnInit(): void {
+    this.fetchData();
+    this.primengConfig.ripple = true;
   }
 
+  fetchData() {
+    this.getListTeacher();
+  }
+
+  getListTeacher(): void {
+    const queryParams = queryString.stringify(this.query);
+    this.apiService
+      .getListTeacher(queryParams)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: (response) => {
+          console.log("Teahcer", response)
+          this.dataList = response.Data.Data;
+          this.totalRecord = response.Data.RecordsTotal;
+        },
+        error: (error) => {
+          console.log('error', error);
+        },
+      });
+  }
+
+  paginate(event: any) {
+    console.log(event);
+    this.query.offSet = event.first;
+    this.query.pageSize = event.rows;
+    this.getListTeacher();
+  }
+
+  deleteProduct(Id: string) {
+    
+  }
+  
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
